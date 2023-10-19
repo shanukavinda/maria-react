8970e673878daebe8862b3c7f336e789ea796976
shanukavinda <kshanuka97@gmail.com>
2023-10-19 10:50:37 +0530

    changed viteconfig  file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      plugins: [commonjs()] // Add the commonjs plugin here
+    }
+  }
+});
